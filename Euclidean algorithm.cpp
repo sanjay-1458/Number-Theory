143dbbbfa50366fa62b50c1ed8ab2abe24297a1c
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-06-03 14:16:05 +0530

    Update Euclidean algorithm.cpp

--- Euclidean algorithm.cpp
+++ Euclidean algorithm.cpp
@@ -7,53 +7,7 @@ using namespace std;
 const int MOD = 1e9 + 7;
 const int INF = LLONG_MAX >> 1;
 
-void gcd1(int a, int b)
-{
-    vector<int> v1, v2;
-    for (int i = 2; i * i <= a; ++i)
-    {
-        while (a % i == 0)
-        {
-            a = a / i;
-            v1.push_back(i);
-        }
-    }
-    for (int i = 2; i * i <= b; ++i)
-    {
-        while (b % i == 0)
-        {
-            b = b / i;
-            v2.push_back(i);
-        }
-    }
-    if (a > 1)
-        v1.push_back(a);
-    if (b > 1)
-        v2.push_back(b);
-    int i = 0, j = 0, res = 1;
-    while (i < v1.size() && j < v2.size())
-    {
-        if (v1[i] == v2[i])
-        {
-            res *= v1[i];
-            i++;
-            j++;
-        }
-        else
-        {
-            break;
-        }
-    }
-    for (auto e : v1)
-        cout << e << " ";
-    cout << endl;
-    for (auto e : v2)
-        cout << e << " ";
-    cout << endl;
-    cout << res << endl;
-}
-
-int gcd2(int a, int b)
+int gcd(int a, int b)
 {
     while (a != 0)
     {
